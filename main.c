3b7f6947dd1fc17cc4edab65dc028ff404372dd3
navid <nhzandi@gmail.com>
2016-11-02 00:12:59 +0330

    After receiving a packet, sends data continuously, but not correct yet

--- main.c
+++ main.c
@@ -18,15 +18,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "lwip/opt.h"
-#include "lwip/arch.h"
-#include "lwip/api.h"
-#include "web.h"
-#include <string.h>
-#include "chregistry.h"
-#include "chprintf.h"
-
-
+#include "ch.h"
 #include "hal.h"
 #include "test.h"
 #include "ch.h"
@@ -35,63 +27,113 @@
 
 #include "web/web.h"
 
+
+#include "lwip/opt.h"
+#include "lwip/arch.h"
+#include "lwip/api.h"
+#include <string.h>
+#include "chregistry.h"
+#include "chprintf.h"
+
+
 #define ADC_GRP1_NUM_CHANNELS   2
 #define ADC_GRP1_BUF_DEPTH      8
 
 static adcsample_t samples1[ADC_GRP1_NUM_CHANNELS * ADC_GRP1_BUF_DEPTH];
 
-#if LWIP_NETCONN
 static struct netconn *conn;
 static struct netbuf *buf;
 static struct ip_addr *addr;
 static unsigned short port;
+
+static void adcerrorcallback(ADCDriver *adcp, adcerror_t err) {
+
+  (void)adcp;
+  (void)err;
+}
+
+static const ADCConversionGroup adcgrpcfg1 = {
+  FALSE,
+  ADC_GRP1_NUM_CHANNELS,
+  NULL,
+  adcerrorcallback,
+  0,                        /* CR1 */
+  ADC_CR2_SWSTART,          /* CR2 */
+  ADC_SMPR1_SMP_AN11(ADC_SAMPLE_3) | ADC_SMPR1_SMP_AN11(ADC_SAMPLE_3),
+  0,                        /* SMPR2 */
+  ADC_SQR1_NUM_CH(ADC_GRP1_NUM_CHANNELS),
+  0,                        /* SQR2 */
+  ADC_SQR3_SQ1_N(ADC_CHANNEL_IN11) | ADC_SQR3_SQ2_N(ADC_CHANNEL_IN12)
+};
+
+
 /**
  * Stack area for the udp thread.
  */
 THD_WORKING_AREA(wa_udp_echo_server, UDP_THREAD_STACK_SIZE);
 
-/**
- * UDP echo server thread.
- */
+ /**
+  * UDP echo server thread.
+  */
 THD_FUNCTION(udp_echo_server, p)
-{
-  err_t err, recv_err;
+ {
+   err_t err, recv_err;
 
-  LWIP_UNUSED_ARG(p);
+   LWIP_UNUSED_ARG(p);
 
-  conn = netconn_new(NETCONN_UDP);
-  if (conn!= NULL)
-  {
-    err = netconn_bind(conn, IP_ADDR_ANY, UDP_THREAD_PORT);
-    if (err == ERR_OK)
-    {
-      while (1)
-      {
-        recv_err = netconn_recv(conn, &buf);
+   conn = netconn_new(NETCONN_UDP);
+   if (conn!= NULL)
+   {
+     err = netconn_bind(conn, IP_ADDR_ANY, UDP_THREAD_PORT);
+     if (err == ERR_OK)
+     {
+       while (1)
+       {
 
-        if (recv_err == ERR_OK)
-        {
-          addr = netbuf_fromaddr(buf);
-          port = netbuf_fromport(buf);
-          netconn_connect(conn, addr, port);
-          buf->addr.addr = 0;
-          netconn_send(conn,buf);
-          netbuf_delete(buf);
-        }
-      }
-    }
-    else
-    {
-      netconn_delete(conn);
-      // printf("can not bind netconn");
-    }
-  }
-  else
-  {
-    // printf("can create new UDP netconn");
-  }
-  // return MSG_OK;
-}
+
+
+         recv_err = netconn_recv(conn, &buf);
+         if (recv_err == ERR_OK)
+         {
+           addr = netbuf_fromaddr(buf);
+           port = netbuf_fromport(buf);
+
+           netconn_connect(conn, addr, port);
+
+           while(1){
+             //  chEvtWaitAny((eventmask_t)1);  //montazer mimune ta yek event ya hamun msg_t az intrupt berese
+             extChannelDisable(&EXTD1, 0); //disable any external interrupt till end of tranmission
+
+             int i = 0;
+             for ( i = 0 ; i < 4 ; i++){
+
+               buf->addr.addr = 0;
+
+               netconn_send(conn,buf);
+              //  netbuf_delete(buf);
+
+             }
+             chThdSleepMilliseconds(5);
+
+
+
+             extChannelEnable(&EXTD1, 0);  //enable ext interrupt to catch again
+           }
+         }
+       }/*end while (1)*/
+     }
+     else
+     {
+       netconn_delete(conn);
+       // printf("can not bind netconn");
+     }
+   }
+   else
+   {
+     // printf("can create new UDP netconn");
+   }
+   // return MSG_OK;
+ }
 
 THD_WORKING_AREA(wa_tcp_echo_server, TCP_THREAD_STACK_SIZE);
 
@@ -164,31 +206,6 @@ THD_FUNCTION(tcp_echo_server,p)
   // return MSG_OK;
 }
 
-#endif /* LWIP_NETCONN */
-
-static void adcerrorcallback(ADCDriver *adcp, adcerror_t err) {
-
-  (void)adcp;
-  (void)err;
-}
-
-static const ADCConversionGroup adcgrpcfg1 = {
-  FALSE,
-  ADC_GRP1_NUM_CHANNELS,
-  NULL,
-  adcerrorcallback,
-  0,                        /* CR1 */
-  ADC_CR2_SWSTART,          /* CR2 */
-  ADC_SMPR1_SMP_AN11(ADC_SAMPLE_3) | ADC_SMPR1_SMP_AN11(ADC_SAMPLE_3),
-  0,                        /* SMPR2 */
-  ADC_SQR1_NUM_CH(ADC_GRP1_NUM_CHANNELS),
-  0,                        /* SQR2 */
-  ADC_SQR3_SQ1_N(ADC_CHANNEL_IN11) | ADC_SQR3_SQ2_N(ADC_CHANNEL_IN12)
-};
-
-
-
-
 /*
  * Orange LED blinker thread, times are in milliseconds.
  */
@@ -204,12 +221,6 @@ static THD_FUNCTION(Thread1, arg)
     palSetPad(GPIOD, GPIOD_LED6);
     chThdSleepMilliseconds(500);
   }
-}
-
-static void led5off(void *arg) {
-
-  (void)arg;
-  palClearPad(GPIOD, GPIOD_LED5);
 }
 
 /********************************************Thread2 implementation*******************************/
@@ -235,8 +246,7 @@ static THD_FUNCTION(Thread2, arg) {
   }
 }
 
-
-/* Triggered when the button is pressed or released. The LED5 is set to ON.*/
+// /* Triggered when the button is pressed or released. The LED5 is set to ON.*/
 static void extcb1(EXTDriver *extp, expchannel_t channel) {
 /*  static virtual_timer_t vt4;
 
@@ -255,7 +265,7 @@ static void extcb1(EXTDriver *extp, expchannel_t channel) {
   (void)channel;
 
   chSysLockFromISR();
-  chEvtSignalI(thread2_p, (eventmask_t)1);  //faal shodan flag baraye thread2
+  // chEvtSignalI(thread2_p, (eventmask_t)1);  //faal shodan flag baraye thread2
   chSysUnlockFromISR();
 }
 
@@ -303,19 +313,10 @@ int main(void)
   halInit();
   chSysInit();
   lwipInit(NULL);
-
   /*
-   * Setting up analog inputs used by the demo.
+   * Activates the serial driver 1 using the driver default configuration.
    */
-  palSetGroupMode(GPIOC, PAL_PORT_BIT(1) | PAL_PORT_BIT(2),
-                  0, PAL_MODE_INPUT_ANALOG);
-
-  
-  /*
-   * our own thread to be called after EXT
-   */
-  thread2_p = chThdCreateStatic(waThread2, sizeof(waThread2), NORMALPRIO, Thread2, NULL); //thread baraye interrupt ba arzeshe normal
-  
+  // sdStart(&SD3, NULL);
 
   /*
    * Creates the blinker thread.
@@ -329,6 +330,12 @@ int main(void)
   //                   lwip_thread, NULL);
 
   /*
+   * our own thread to be called after EXT
+   */
+  thread2_p = chThdCreateStatic(waThread2, sizeof(waThread2), NORMALPRIO, Thread2, NULL); //thread baraye interrupt ba arzeshe normal
+
+
+  /*
    * Creates the UDP echo thread (it changes priority internally).
    */
   chThdCreateStatic(wa_udp_echo_server, sizeof(wa_udp_echo_server), NORMALPRIO + 1,
@@ -340,6 +347,7 @@ int main(void)
    */
   chThdCreateStatic(wa_tcp_echo_server, sizeof(wa_tcp_echo_server), NORMALPRIO + 1,
                     tcp_echo_server, NULL);
+
 
   /*
    * Activates the ADC1 driver and the temperature sensor.
